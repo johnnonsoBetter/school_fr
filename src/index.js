8dd60beb5f234cfedb914628692a5ce0b9a9bf5f
John <johnnonso090@gmail.com>
2021-11-04 21:14:15 +0100

    created a swconfig file to active service worker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import swConfig from './swConfig';
 
 
 ReactDOM.render(
@@ -21,7 +22,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register(swConfig);
 
  
 // If you want to start measuring performance in your app, pass a function
